b45283814953586b245fb8bdd2667ee1b1b6cc0c
Adil Aidyn <adamaydin915@gmail.com>
2022-10-26 11:09:37 +0600

    Initial Commit

---
+++ DiceAppSwiftUI/DiceAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceAppSwiftUIApp.swift
+//  DiceAppSwiftUI
+//
+//  Created by Adm Aidyn on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
