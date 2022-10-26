0f7a262fdda44787a04fa8c01b7dd771692ef6cb
Adil Aidyn <adamaydin915@gmail.com>
2022-10-26 12:32:35 +0600

    Make fully working app

--- DiceAppSwiftUI/ContentView.swift
+++ DiceAppSwiftUI/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var leftDiceNumber = 1
+    @State var rightDicenumber = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Rectangle()
+                .foregroundColor(.green)
+                .ignoresSafeArea()
+            VStack {
+                Text("Dice App")
+                    .font(.system(size: 30, weight: .bold, design: .monospaced))
+                    .foregroundColor(.white)
+                    .padding()
+                Spacer()
+                HStack {
+                    Image("dice\(leftDiceNumber)")
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .padding()
+                    Image("dice\(rightDicenumber)")
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .padding()
+                }
+                Spacer()
+                
+                Button(action:{
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDicenumber = Int.random(in: 1...6)
+                }) {
+                    Text("Shuffle")
+                        .font(.system(size: 30, weight: .bold, design: .rounded))
+                        .fontWeight(.medium)
+                        .foregroundColor(.white)
+                        .padding()
+                }
+                .background(.red)
+                .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+            }
+        }
     }
 }
 
